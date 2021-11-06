086b696906dc33305ab6e5bdb5f7020c1618a854
Jonah <tgtechie01@gmail.com>
2021-11-06 06:45:14 -0400

    programmed peripheral sans  the led bits (added stubbed funciton with print instead)

--- MetisShelf/src/main.cpp
+++ MetisShelf/src/main.cpp
@@ -5,63 +5,182 @@
 #include <EEPROM.h>
 #include <Wire.h>
 
-// Cunction Prototype for the I2C peripheral event handler
-void onI2CWrite(int);
-void onI2CRead();
+// prototypes for code organization
+void setup_peripherals(void);
+bool uidIsValid(void);
+UID getUID(void);
+int8_t getUIDByte(int);
+void setUIDByte(int, uint8_t);
+uint8_t readReg(uint8_t);
+
+typedef int Drawer;
+void writeDrawer(Drawer* drawer, uint8_t col, uint8_t row, bool onOff);
 
 // --- hardware / pin names ---
+#define WAKE_DELAY 100
 #define INDICATOR LED_BUILTIN
+#define NUM_DRAWERS_WIDE 5
+#define NUM_DRAWERS_HIGH 4
+#define CLEAR_UID_ON_BOOT 0
 
-int8_t assigned_addr = -1;  // -1 is not a valid addr as the top bit is high
+// --- register globals ---
+uint8_t selectedCol = 0;
+uint8_t selectedRow = 0;
 
+// --- the nitty gritty ---
 void setup() {
+    setup_peripherals();
+
+    // dubug purposes
+    Serial.print("UID: ");
+    Serial.println(getUID());
+
+    // send an addr request to master
+    Wire.beginTransmission(HUB_ADDR);
+    Wire.write(HUB_ENROL_REQ);
+    Wire.endTransmission();
+}
+
+void loop() {
+    delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void onI2CWrite(int nBytesRxed) {
+    int x = Wire.read();    // receive byte as an integer
+    Serial.print((char)x);  // print the integer
+}
+
+void onI2CRead() {
+    if (Wire.available() != 1) {
+        Serial.print("all the read requests should be 1 byte long, got ");
+        Serial.print(Wire.available());
+        Serial.println(" bytes.");
+    }
+    // the addr they are requesting
+    uint8_t regAddr = Wire.read();
+    Wire.write(readReg(regAddr));
+}
+
+void setDeviceAddress(uint8_t addr) {
+    Wire.begin(addr);
+    // register the I2C Handlers
+    Wire.onReceive(onI2CWrite);
+    Wire.onRequest(onI2CRead);
+}
+
+uint8_t readDrawerStatus(int x) {
+    return 69;
+}
+
+uint8_t readReg(uint8_t regAddr) {
+    switch (regAddr) {
+
+    case SHELF_UID_B0: return getUIDByte(0);
+    case SHELF_UID_B1: return getUIDByte(1);
+    case SHELF_UID_B2: return getUIDByte(2);
+    case SHELF_UID_B3: return getUIDByte(4);
+    case SHELF_DRAWERS_WIDE: return NUM_DRAWERS_WIDE;
+    case SHELF_DRAWERS_HIGH: return NUM_DRAWERS_HIGH;
+    case SHELF_SEL_COL: return selectedCol;
+    case SHELF_SEL_ROW: return selectedRow;
+    case SHELF_DRAWER_STAT:
+        return readDrawerStatus((selectedCol << 8) | selectedRow);
+    default: break;
+    }
+}
+
+void writeReg(uint8_t regAddr, uint8_t value) {
+    int drawer = 0;
+    switch (regAddr) {
+    case SHELF_UID_B0: setUIDByte(0, value);
+    case SHELF_UID_B1: setUIDByte(1, value);
+    case SHELF_UID_B2: setUIDByte(2, value);
+    case SHELF_UID_B3: setUIDByte(3, value);
+
+    case SHELF_I2C_ADDR: setDeviceAddress(value);
+    case SHELF_SEL_COL: selectedCol = value;
+    case SHELF_SEL_ROW: selectedRow = value;
+    case SHELF_DRAWER_STAT:
+        writeDrawer(&drawer, selectedCol, selectedRow, (bool)value);
+    case SHELF_CMD: {
+        Serial.print("turn all leds to ");
+        Serial.println(value);
+    }
+    default: break;
+    }
+}
+
+void setup_peripherals(void) {
     // this is for dev purposes
     Serial.begin(9600);  // start serial for output
-    Serial.print("initing...");
+    Serial.print("initing peripherals...");
 
     // --- boot up ---
     // on boot we need to wait a bit as it is possible we were just hot plugged
     // into  a network while we are waitng we turn on the D13 LED
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(INDICATOR, HIGH);
-    delay(2000);
+    delay(WAKE_DELAY);
     digitalWrite(INDICATOR, LOW);
 
     // --- I2C main / peripheral handlers / etc ---
     // once we have done that we boot up the I2C peripheral, this also makes it
     // a I2C controller However since this has yet to be assigned an address as
     // it boots as in unknown device
-    Wire.begin(UNASSIGNED_DEV_ADDR);  // join i2c bus with address #4
-    // register the I2C Handlers
-    Wire.onReceive(onI2Write);
-    Wire.onRequest(onI2CRead);
+    setDeviceAddress(UNASSIGNED_SHELF_ADDR);
+
+    EEPROM.begin();
 
     // ...
     Serial.println(" done!");
-}
 
-void loop() {
-    auto msg = Serial.read();
-    if (msg != -1) {
-        Serial.print((char)msg);
-
-        Wire.beginTransmission(4);  // transmit to device #4
-        Wire.write((char)msg);      // sends five bytes
-        //  Wire.write(x);              // sends one byte
-        Wire.endTransmission();  // stop transmitting
+    if (CLEAR_UID_ON_BOOT) {
+        Serial.println("clearing UID");
+        setUIDByte(0, 0);
+        setUIDByte(1, 0);
+        setUIDByte(2, 0);
+        setUIDByte(3, 0);
     }
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-void onI2CWrite(int nBytesRxed) {
-    // while (1 < Wire.available())  // loop through all but the last
-    // {
-    //     char c = Wire.read();  // receive byte as a character
-    //     Serial.print(c);       // print the character
-    // }
-    int x = Wire.read();    // receive byte as an integer
-    Serial.print((char)x);  // print the integer
+bool uidIsValid(void) {
+    return (bool)EEPROM[0];
 }
 
-void onI2CRead() {}
+UID getUID(void) {
+    return ((EEPROM[3] << 24) | (EEPROM[2] << 16)) |
+           ((EEPROM[1] << 8) | (EEPROM[0] << 0));
+}
+
+int8_t getUIDByte(int addr) {
+    /// -1 return is means invalid addr requested, but just don't do that please
+    if (addr < 0 || addr > 3) {
+        return -1;
+    } else {
+        return EEPROM[addr];
+    }
+}
+
+void setUIDByte(int byteIndex, uint8_t value) {
+    if (byteIndex < 0 || byteIndex > 3) {
+        Serial.print("invalid UID byte addr, tried to write to");
+        Serial.println(byteIndex);
+        return;
+    } else {
+        EEPROM.write(byteIndex, value);
+    }
+}
+
+void writeDrawer(Drawer* drawer, uint8_t col, uint8_t row, bool onOff) {
+    Serial.print("set led ");
+    Serial.print(col);
+    Serial.print(" over and ");
+    Serial.print(row);
+    Serial.print(" down ");
+    if (onOff)
+        Serial.println("on");
+    else
+        Serial.println("off");
+}
--- MetisShelfProtocol.h
+++ MetisShelfProtocol.h
@@ -1,3 +1,6 @@
+#include <stdint.h>
+
+typedef uint32_t UID;
 
 /// addr assignments
 #define HUB_ADDR 0x40
@@ -7,23 +10,23 @@
 
 /// Hub Register map
 /// >>> 0xff : enroll, start enrolling new device
-#define HUB_ENROL_REQ 0x00
+#define HUB_ENROL_REQ 0x69
 
 /// Shelf Register Map (working addrs)
 /// >>> UID / Device data
-#define SHELF_UID_B0 0xb0        /// r/w : uid byte 0
-#define SHELF_UID_B1 0xb1        /// r/w : uid byte 1
-#define SHELF_UID_B2 0xb2        /// r/w : uid byte 2
-#define SHELF_UID_B3 0xb3        /// r/w : uid byte 3
+#define SHELF_UID_B0 0xb0  /// r/w : uid byte 0
+#define SHELF_UID_B1 0xb1  /// r/w : uid byte 1
+#define SHELF_UID_B2 0xb2  /// r/w : uid byte 2
+#define SHELF_UID_B3 0xb3  /// r/w : uid byte 3
+
 #define SHELF_DRAWERS_WIDE 0xa0  /// r   : width of shelf (in drawers)
 #define SHELF_DRAWERS_HIGH 0xa1  /// r   : height of shelf (in drawers)
 #define SHELF_I2C_ADDR 0xad      /// w   : device address (init=unassigned addr)
 /// >>> drawer addressing
-#define SHELF_UID_ 0xc0  /// r/w : current drawer horizontal index
-#define SHELF_UID_ 0xc1  /// r/w : current drawer vertical index
+#define SHELF_SEL_COL 0xc0      /// r/w : current drawer horizontal index
+#define SHELF_SEL_ROW 0xc1      /// r/w : current drawer vertical index
+#define SHELF_DRAWER_STAT 0xcf  /// r/w: bit 0 is the led on/off indc
 /// >>> command addr
 #define SHELF_CMD 0xff  /// w : command reg (
-///     - 0x00 drawer off
-///     - 0x01 drawer on
 ///     - 0xf0 all off
 ///     - 0xf1 all on
